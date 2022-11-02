3924117efc77a5b026d7a2ff09a7cc60b1e14ac5
Rodolfo Giometti <giometti@enneenne.com>
2022-11-02 18:45:14 +0100

    print.h: drop not used pr_<type>() functions
    This patch is need to start a complete rework of the daemon's messages
    system.
    
    Signed-off-by: Rodolfo Giometti <giometti@enneenne.com>

--- print.h
+++ print.h
@@ -15,13 +15,8 @@ __printf(2, 3)
 #endif
 void print(int level, char const *format, ...);
 
-#define pr_emerg(x...)   print(LOG_EMERG, x)
 #define pr_alert(x...)   print(LOG_ALERT, x)
-#define pr_crit(x...)    print(LOG_CRIT, x)
 #define pr_err(x...)     print(LOG_ERR, x)
-#define pr_warning(x...) print(LOG_WARNING, x)
-#define pr_notice(x...)  print(LOG_NOTICE, x)
 #define pr_info(x...)    print(LOG_INFO, x)
-#define pr_debug(x...)   print(LOG_DEBUG, x)
 
 #endif /* PRINT_H */
